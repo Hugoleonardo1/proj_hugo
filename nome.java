bb8b05c192f95078e8eb1aadd09b7f92a17a034d
HUGO12345h <hugoleonardogomes12@gmail.com>
2025-02-19 11:56:47 -0300

    Meu segundo projeto

--- nome.java
+++ nome.java
@@ -1,9 +1,39 @@
 package Projetos_LPA.aula04;
 
+import java.util.Scanner;
+
 public class nome {
 
 	public static void main(String[] args) {
         System.out.println("Hugo Leonardo Gomes Rodrigues");
+        // Variáveis
+        Double ht, vh, sb, sl, inss;
+        
+        // Instaciar class Scanner
+        Scanner ler = new Scanner(System.in);
+        
+        // Entrada de dados
+        System.out.print("Quantas horas você trabalha por dia: ");
+        ht = ler.nextDouble();
+        
+        System.out.print("Quanto você ganha por hora: ");
+        vh = ler.nextDouble();
+        
+        // Processamento
+        sb = ht*vh;
+        
+        inss = sb*0.12;
+        
+        sl = sb-inss;
+        
+        // Saída
+        System.out.print("O valor do salário bruto é: " + sb);
+       System.out.print("O valor do salário líquido é: " + sl);
+        		
+        		
+        
+        
+        
 	}
 
 }
